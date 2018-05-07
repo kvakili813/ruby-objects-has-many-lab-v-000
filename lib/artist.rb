e1e37486d829047c0b55a1446ecb863408c3860f
Kayvan Vakili <kvakili813@gmail.com>
2018-05-07 21:14:59 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,6 +12,7 @@ attr_accessor :name
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
@@ -21,7 +22,7 @@ attr_accessor :name
   end
 
   def self.song_count
-    @@song_count.all
+    @@song_count
   end
 
   def songs
